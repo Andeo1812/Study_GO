6acef6f90927ded0b5f89d81e8c68e92b5dca8ee
Andeo1812 <alexanforall@gmail.com>
2022-09-17 13:51:46 +0300

    end

--- project/calc/parser/actions/calculate.go
+++ project/calc/parser/actions/calculate.go
@@ -1,9 +1,6 @@
 package actions
 
-import "fmt"
-
 func InitCalculate(expression string) (float64, error) {
-	fmt.Println("----------------")
 	res, _, err := actionSum(0, expression)
 	return res, err
 }
